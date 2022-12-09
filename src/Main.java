8ae4940c47a50f0bf4f8cf3f74c5006beda5dfde
HackathonJava <hackathon.hava.22@gmail.com>
2022-12-09 22:22:52 +0600

    add shifr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
